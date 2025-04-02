348e0e99eec003f421009f14ee7de221ca074742
Pompey06 <dmytrouryvskyi@gmail.com>
2025-04-02 18:35:48 +0200

    wip

--- js/console.js
+++ js/console.js
@@ -1,8 +1,8 @@
 new Swiper(".console__swiper", {
-  navigation: {
-    nextEl: ".swiper__next",
-    prevEl: ".swiper__prev",
-  },
+   navigation: {
+      nextEl: ".swiper__next",
+      prevEl: ".swiper__prev",
+   },
 });
 
 const consoleWrap = document.querySelector("[data-console]");
@@ -10,116 +10,116 @@ const consoleFirst = document.querySelector(".console__first");
 const consoleLast = document.querySelector(".console__last");
 
 // btns
-function initBtns() { 
-  const btns = document.querySelectorAll("[data-btn]");
-  btns.forEach((btn) => {
-    const onHandlePress = btn.getAttribute("data-btn");
-    onPressWithRelease(
-      btn,
-      () => {
-        btn.classList.add("_active");
-        if (typeof window[onHandlePress] === "function") {
-          window[onHandlePress]();
-        }
-      },
-      () => {
-        btn.classList.remove("_active");
-      },
-    );
-  });
+function initBtns() {
+   const btns = document.querySelectorAll("[data-btn]");
+   btns.forEach((btn) => {
+      const onHandlePress = btn.getAttribute("data-btn");
+      onPressWithRelease(
+         btn,
+         () => {
+            btn.classList.add("_active");
+            if (typeof window[onHandlePress] === "function") {
+               window[onHandlePress]();
+            }
+         },
+         () => {
+            btn.classList.remove("_active");
+         }
+      );
+   });
 }
 
 function initDradBtns() {
-  const dpad = document.querySelector("[data-dpad]");
-  const dpadNext = document.querySelector("[data-dpad-next]");
-  const dpadPrev = document.querySelector("[data-dpad-prev]");
+   const dpad = document.querySelector("[data-dpad]");
+   const dpadNext = document.querySelector("[data-dpad-next]");
+   const dpadPrev = document.querySelector("[data-dpad-prev]");
 
-  const swiperNext = document.querySelector(".swiper__next");
-  const swiperPrev = document.querySelector(".swiper__prev");
+   const swiperNext = document.querySelector(".swiper__next");
+   const swiperPrev = document.querySelector(".swiper__prev");
 
-  onPressWithRelease(
-    dpadNext,
-    () => {
-      dpad.classList.add("_next");
-      swiperNext.click();
-      showSwiperScreen();
-    },
-    () => {
-      dpad.classList.remove("_next");
-    },
-  );
+   onPressWithRelease(
+      dpadNext,
+      () => {
+         dpad.classList.add("_next");
+         swiperNext.click();
+         showSwiperScreen();
+      },
+      () => {
+         dpad.classList.remove("_next");
+      }
+   );
 
-  onPressWithRelease(
-    dpadPrev,
-    () => {
-      dpad.classList.add("_prev");
-      swiperPrev.click();
-      showSwiperScreen();
-    },
-    () => {
-      dpad.classList.remove("_prev");
-    },
-  );
+   onPressWithRelease(
+      dpadPrev,
+      () => {
+         dpad.classList.add("_prev");
+         swiperPrev.click();
+         showSwiperScreen();
+      },
+      () => {
+         dpad.classList.remove("_prev");
+      }
+   );
 }
 
-function initConsole() { 
-  showStartScreen();
+function initConsole() {
+   showStartScreen();
 }
 
 function onPressWithRelease(el, onPressCallback, onReleaseCallback) {
-  // press
-  el.addEventListener("touchstart", () => {
-    onPressCallback();
-  });
+   // press
+   el.addEventListener("touchstart", () => {
+      onPressCallback();
+   });
 
-  el.addEventListener("mousedown", () => {
-    onPressCallback();
-  });
+   el.addEventListener("mousedown", () => {
+      onPressCallback();
+   });
 
-  // release
-  el.addEventListener("touchend", () => {
-    onReleaseCallback();
-  });
+   // release
+   el.addEventListener("touchend", () => {
+      onReleaseCallback();
+   });
 
-  el.addEventListener("mouseup", () => {
-    onReleaseCallback();
-  });
+   el.addEventListener("mouseup", () => {
+      onReleaseCallback();
+   });
 }
 
 function showStartScreen() {
-  consoleWrap.classList.add("_start");
-  consoleWrap.classList.remove("_show_swiper");
-  consoleWrap.classList.remove("_trailer");
+   consoleWrap.classList.add("_start");
+   consoleWrap.classList.remove("_show_swiper");
+   consoleWrap.classList.remove("_trailer");
 }
 
 function showTrailerScreen() {
-  consoleWrap.classList.add("_trailer");
-  consoleWrap.classList.remove("_show_swiper");
-  consoleWrap.classList.remove("_start");
+   consoleWrap.classList.add("_trailer");
+   consoleWrap.classList.remove("_show_swiper");
+   consoleWrap.classList.remove("_start");
 }
 
 function showSwiperScreen() {
-  consoleWrap.classList.add("_show_swiper");
-  consoleWrap.classList.remove("_start");
-  consoleWrap.classList.remove("_trailer");
+   consoleWrap.classList.add("_show_swiper");
+   consoleWrap.classList.remove("_start");
+   consoleWrap.classList.remove("_trailer");
 }
 
 function onStartConsole() {
-  console.log("start");
-  showStartScreen();
+   console.log("start");
+   showSwiperScreen();
 }
 
 function onTrailerConsole() {
-  console.log("trailer");
-  showTrailerScreen();
+   console.log("trailer");
+   showTrailerScreen();
 }
 
 function onPressA() {
-  console.log("press a");
+   console.log("press a");
 }
 
 function onPressB() {
-  console.log("press b");
+   console.log("press b");
 }
 
 initDradBtns();
